70af5ac05ceecf7f954e6fb631395afb5e12d60a
BeatrizDile <beatriz.dile@gmail.com>
2022-08-30 17:09:30 -0300

    Day 1

---
+++ Section-1-1/day-1-4-variables/main.py
@@ -0,0 +1,13 @@
+name = input('What is your name? ')
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
---
+++ Section-1-1/day-1-6-final/main.py
@@ -0,0 +1,3 @@
+print('Welcome to the Band Name Generator')
+
+print("Your band name could be "+input("What's name of the city you grew up in?\n")+" "+input("What's your pet's name?\n"))
