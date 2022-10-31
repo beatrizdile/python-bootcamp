84f437ddfc675360c1bf3c4909a214fedc31c432
BeatrizDile <beatriz.dile@gmail.com>
2022-10-31 12:59:46 -0300

    Day 41

--- Section-5-41/day-41-flask-framework/hello.py
+++ Section-5-41/day-41-flask-framework/hello.py
@@ -5,11 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Hello, World!</h1>'
+
 
 @app.route("/bye")
 def say_bye():
     return "Bye"
 
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello {name}, you are {number} years old!"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
