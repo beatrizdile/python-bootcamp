b868083cd4e9470a77e11d1712ded0f7defb188c
BeatrizDile <beatriz.dile@gmail.com>
2022-11-13 18:01:46 -0300

    Day 42

---
+++ Section-5-42/day-42-sqlite/main.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+# make connection
+conn = sqlite3.connect("customer.db")
+
+# create cursor
+c = conn.cursor()
+
+# create table
+# c.execute("""CREATE TABLE customers (
+#         first_name text,
+#         last_name text,
+#         email text
+#     )""")
+
+# insert
+# c.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@gmail.com')")
+
+# insert multiple data
+many_customer = [('Wes', 'Brown', 'wes@gmail.com'),
+                 ('Jules', 'Snow', 'jules@gmail.com'),
+                 ('Mary', 'White', 'mary@gmail.com')]
+
+c.execute("INSERT INTO customers VALUES (?,?,?)", many_customer)
+
+# commit
+conn.commit()
+
+# close connection
+conn.close()
