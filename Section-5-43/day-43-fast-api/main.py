625ff30b558edb197d8b9420edc05f2e1f54a664
BeatrizDile <beatriz.dile@gmail.com>
2022-11-26 17:45:22 -0300

    Day 43

--- Section-5-43/day-43-fast-api/main.py
+++ Section-5-43/day-43-fast-api/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,21 +12,51 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
     print(post)
     print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
+    return {"post_detail": post}
+
+
 
 # title str, content str, category
+# venv\Scripts\activate.bat
+# uvicorn main:app --reload
+# ctrl+k+c / ctrl+k+u
 
 
 
