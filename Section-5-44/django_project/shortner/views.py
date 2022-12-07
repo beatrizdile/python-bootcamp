60d15ce8bcc4a38657bcbe0edafeb0ba94db8de7
BeatrizDile <beatriz.dile@gmail.com>
2022-12-07 18:05:23 -0300

    Day 44

--- Section-5-44/django_project/shortner/views.py
+++ Section-5-44/django_project/shortner/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+import uuid
+from .models import Url
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
