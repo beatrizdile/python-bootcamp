636ac41f308c0e3769d9da5f94693e209e7cdd3d
BeatrizDile <beatriz.dile@gmail.com>
2022-12-10 09:34:30 -0300

    Day 44

--- Section-5-44/mysite/mysite/settings.py
+++ Section-5-44/mysite/mysite/settings.py
@@ -15,12 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=$lx@mg)j+ae4e32^vl383k2_fm%m^7ksgp2-uvo=+a!sb0cq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
