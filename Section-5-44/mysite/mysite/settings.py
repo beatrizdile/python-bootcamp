76cf4e2155e992c81e5869fbbbda1c98ae8796b7
Beatriz Dile <beatrix.dile@gmail.com>
2022-12-13 07:20:53 -0300

    Day 44

--- Section-5-44/mysite/mysite/settings.py
+++ Section-5-44/mysite/mysite/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -33,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
