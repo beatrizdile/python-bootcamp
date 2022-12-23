dae412bd088ad100e42015de12695a193cef0c24
Beatriz Dile <beatrix.dile@gmail.com>
2022-12-23 16:56:25 -0300

    Day 45

--- Section-5-45/day-45-tetris/main.py
+++ Section-5-45/day-45-tetris/main.py
@@ -164,6 +164,15 @@ def convert_shape_format(shape):
     positions = []
     format = shape.shape[shape.rotation % len(shape.shape)]
 
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
 
 def valid_space(shape, grid):
     pass
