2b6614bc9da697212a8d0a752e683ff12daf354f
Beatriz Dile <beatrix.dile@gmail.com>
2022-12-29 18:31:11 -0300

    Day 45

--- Section-5-45/day-45-tetris/main.py
+++ Section-5-45/day-45-tetris/main.py
@@ -188,7 +188,12 @@ def valid_space(shape, grid):
 
 
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+
+    return False
 
 
 def get_shape():
