01d1d4e8e3284a0989645281a8eb38c0202aa2d2
Beatriz Dile <beatrix.dile@gmail.com>
2023-01-03 15:57:18 -0300

    Day 45

--- Section-5-45/day-45-tetris/main.py
+++ Section-5-45/day-45-tetris/main.py
@@ -220,7 +220,21 @@ def clear_rows(grid, locked):
     pass
 
 def draw_next_shape(shape, surface):
-    pass
+    font = pygame.font.SysFont('helvetica', 30)
+    label = font.render('Next Shape', 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy + i*block_size, block_size, block_size), 0)
+    surface.blit(label, (sx + 10, sy - 30))
+
+
 
 def draw_window(surface, grid):
     surface.fill((0, 0, 0))
@@ -239,7 +253,6 @@ def draw_window(surface, grid):
 
     draw_grid(surface, grid)
 
-    pygame.display.update()
 
 def main(win):
 
@@ -307,6 +320,8 @@ def main(win):
             change_piece = False
 
         draw_window(win, grid)
+        draw_next_shape(next_piece, win)
+        pygame.display.update()
 
         if check_lost(locked_positions):
             run = False
