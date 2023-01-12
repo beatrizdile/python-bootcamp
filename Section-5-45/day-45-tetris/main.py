beac1ca6273e992cc51a268552d2429905ae5e78
Beatriz Dile <beatrix.dile@gmail.com>
2023-01-12 11:41:06 -0300

    Day 45

--- Section-5-45/day-45-tetris/main.py
+++ Section-5-45/day-45-tetris/main.py
@@ -257,7 +257,7 @@ def draw_next_shape(shape, surface):
 
 
 
-def draw_window(surface, grid):
+def draw_window(surface, grid, score=0):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
@@ -265,6 +265,14 @@ def draw_window(surface, grid):
     label = font.render('Tetris', True, (255, 255, 255))
 
     surface.blit(label, (top_left_x + play_width / 2 - label.get_width() / 2, 30))
+
+    font = pygame.font.SysFont('helvetica', 30)
+    label = font.render('Score: ' + str(score), 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+
+    surface.blit(label, (sx + 25, sy + 140))
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -348,9 +356,9 @@ def main(win):
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
-            clear_rows(grid, locked_positions)
+            score += clear_rows(grid, locked_positions) * 10
 
-        draw_window(win, grid)
+        draw_window(win, grid, score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
