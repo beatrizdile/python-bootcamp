39decf319d1f1934c51a23e34dd4b221697085de
Beatriz Dile <beatrix.dile@gmail.com>
2023-01-23 11:22:54 -0300

    Day 47

---
+++ Section-5-47/day-47-space-invaders/main.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+import time
+import random
+
+#load imgs
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+#player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+#lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# background
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
