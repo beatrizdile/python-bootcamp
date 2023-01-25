bb16e03a233f4965f8fe1a1da8e91247108d31f1
Beatriz Dile <beatrix.dile@gmail.com>
2023-01-25 18:19:15 -0300

    Day 47

--- Section-5-47/day-47-space-invaders/main.py
+++ Section-5-47/day-47-space-invaders/main.py
@@ -3,6 +3,10 @@ import os
 import time
 import random
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
 #load imgs
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -19,3 +23,18 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
