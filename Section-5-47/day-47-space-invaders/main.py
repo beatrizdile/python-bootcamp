c979d3ae21122eb5c47de4a4e70b6f13eacea608
Beatriz Dile <beatrix.dile@gmail.com>
2023-01-28 16:33:20 -0300

    Day 47

--- Section-5-47/day-47-space-invaders/main.py
+++ Section-5-47/day-47-space-invaders/main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -28,10 +29,21 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("helvetica", 50)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0,0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", True, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", True, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
         pygame.display.update()
 
     while run:
