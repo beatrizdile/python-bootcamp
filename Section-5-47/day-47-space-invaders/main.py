f8e3f637d050127abe952d0a1e5228b2d23069f1
BeatrizDile <beatriz.dile@gmail.com>
2023-01-31 19:15:23 -0300

    Day 47

--- Section-5-47/day-47-space-invaders/main.py
+++ Section-5-47/day-47-space-invaders/main.py
@@ -30,6 +30,14 @@ class Ship:
         self.x = x
         self.y = y
         self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
+
 
 
 def main():
