62683a7c4bd59eed253e2bf10284033ec848378c
BeatrizDile <beatriz.dile@gmail.com>
2023-02-16 19:02:58 -0300

    Day 47

--- Section-5-47/day-47-space-invaders/main.py
+++ Section-5-47/day-47-space-invaders/main.py
@@ -127,6 +127,12 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT:  # down
             player.y += player_vel
 
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
         redraw_window()
 
 main()
